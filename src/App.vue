9ed9850bfc93d5180cce9cecba92ea0900f05651
zhouhang <1728451471@qq.com>
2018-04-22 20:17:34 +0800

    第一次提交

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<!--1模板：html结构-->
+<template>
+  <div id="app">
+  </div>
+</template>
+
+<!--2行为：处理逻辑-->
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<!--3样式：解决样式-->
+<style>
+</style>
