fdd483257c937f9df0fdf7dde7e2dbb5fef74b1d
zhouhangde <1728451471@qq.com>
2019-02-26 14:17:34 +0800

    连接数据库

--- server/config/db.js
+++ server/config/db.js
@@ -3,7 +3,7 @@
 const Sequelize = require('sequelize'); // 引入sequelize
 
 // 使用url连接的形式进行连接，注意将root: 后面的XXXX改成自己数据库的密码
-const Todolist = new Sequelize('mysql://root:maihe@localhost/todolist',{
+const Todolist = new Sequelize('mysql://root:root@127.0.0.1/todolist',{
   define: {
     timestamps: false // 取消Sequelzie自动给数据表加入时间戳（createdAt以及updatedAt）
   }
